a1d7327dea81272ee66c523f962caf8b06c9995b
Midas Wouters <midas@chechi.be>
2014-09-09 17:39:03 +0200

    -

--- cpvs_view.h
+++ cpvs_view.h
@@ -17,18 +17,25 @@ public:
 		psdl::block* pBlock = m_pPSDL->get_block(iBlock);
 		if (!pBlock) return;
 
-		glColor3f(.0f, .0f, 1.f);
+		psdl::vertex v;
 
+		glDisable(GL_TEXTURE_2D);
+		glColor3f(.0f, .0f, 1.f);
 		glLineWidth(2.f);
-		glBegin(GL_LINE_LOOP);
+
+		glBegin(GL_QUAD_STRIP);
 
 		for (unsigned long iPt = 0; iPt < pBlock->num_perimeters(); iPt++)
 		{
-			glVertex3fv(&(m_pPSDL->get_vertex(pBlock->_perimeter[iPt].vertex)->x));
+			v = *m_pPSDL->get_vertex(pBlock->_perimeter[iPt].vertex);
+			glVertex3fv(&(v.x));
+			glVertex3f(v.x, v.y + SW_H, v.z);
 		}
 
 		glEnd();
+
 		glLineWidth(1.f);
+		glEnable(GL_TEXTURE_2D);
 	}
 
 	void SetPSDL(psdl* pPSDL)
